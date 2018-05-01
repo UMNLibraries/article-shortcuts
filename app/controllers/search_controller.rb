9a91bf9c7d0aee81e3b279a75fa3876e077660bd
Michael Berkowski <mjb@umn.edu>
2018-05-01 11:03:08 -0500

    Logic update begins parsing citation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,23 +3,36 @@ require 'anystyle'
 
 class SearchController < ApplicationController
   def search
-    if params[:q]
-      # Remove leading,trailing newlines and trailing dots
-      q = params[:q].strip.sub /\.*\z/, ''
-      @dois = doi(q)
-      # Query by list of extracted DOIs or by normal search
-      es_body = @dois.empty? ? es_query(params[:q]) : es_by_dois(@dois)
-      result = es.search index: 'article', body: es_body
-      article_results = result['hits']['hits'].map { |r| ArticleResult.new r }
-      # This is patched through ActiveModel::Serializers for JSON:API serialization
-      render json: article_results
+    #render(nothing: true, status: :bad_request) unless params[:q].to_s.length > 0
+
+    # Remove leading,trailing newlines and trailing dots
+    q = params[:q].squish.strip.sub /\.*\z/, ''
+    @dois = doi(q)
+    # Query by list of extracted DOIs or by normal search
+    es_body = if !@dois.empty?
+      es_by_dois(@dois)
+    else
+      cite = parse_cite q
+      #if cite[:author]
+      es_by_any(params[:q])
     end
+    result = es.search index: 'article', body: es_body
+    article_results = result['hits']['hits'].map { |r| ArticleResult.new r }
+    # This is patched through ActiveModel::Serializers for JSON:API serialization
+    render json: article_results
   end
 
   protected
   # Returns a SimpleDOI::DOI if one is found in the input string
   def doi(search)
     SimpleDOI.extract(search).map { |d| d.to_s.upcase }
+  end
+
+  def parse_cite(search)
+    # Default parser will attempt to separate on newlines
+    # which does not work well for
+    @parser ||= AnyStyle::Parser.new
+    @parser.parse search
   end
 
   def es
@@ -37,22 +50,37 @@ class SearchController < ApplicationController
     }
   end
 
-  def es_query(search, size=5)
+  def es_by_any(search, size=5)
+    {
+      from: 0,
+      size: size,
+      min_score: "50",
+      query: {
+        simple_query_string: {
+          query: search,
+          #fields: ['article.title^2', 'article.authors.name.surname']
+          fields: ['article.title^2',]
+        }
+      }
+    }
+  end
+
+  def es_by_title_author(title, author, size=5)
     {
       from: 0,
       size: size,
       query: {
-        bool: { 
+        bool: {
           should: [
             {match: {
-              'article.title' => {query: search, operator: 'and', boost: 2}
+              'article.title' => {query: title, operator: 'and', boost: 2}
             }},
             {
               nested: {
                 path: 'article.authors',
                 query: {
                   match: {
-                    'article.authors.name.surname' => {query: search}
+                    'article.authors.name.surname' => {query: author}
                   }
                 }
               }
