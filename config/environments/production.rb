41387b841af5ebeeec6a9aea1a842d3865830bd7
Michael Berkowski <mjb@umn.edu>
2018-05-10 13:18:43 -0500

    some asset config for prod, not yet needed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,11 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Do not hash asset names in the end. Minify them but no fingerprint
+  # Not sure this actually works....
+  config.assets.digest = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
